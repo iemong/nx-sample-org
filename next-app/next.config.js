29ee77b182d5ef4e57a50a9f41d369df6219f0d7
iemong <kiyoshidainagon@gmail.com>
2023-10-30 14:35:44 +0900

    fix

--- next-app/next.config.js
+++ next-app/next.config.js
@@ -7,6 +7,8 @@ const { composePlugins, withNx } = require('@nx/next');
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
  **/
 const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
   nx: {
     // Set this to true if you would like to use SVGR
     // See: https://github.com/gregberge/svgr
